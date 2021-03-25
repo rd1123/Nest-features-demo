70919d8f2d59f79cb3bf05fd9746e64651e074be
rd <rdding1123@gmail.com>
2021-03-25 10:31:38 +0800

    Create middleware demo

--- src/check-middleware.middleware.ts
+++ src/check-middleware.middleware.ts
@@ -1,8 +1,14 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { Request, Response } from 'express';
 
 @Injectable()
-export class CheckMiddlewareMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
-    next();
+export class CheckMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: () => void) {
+    if(req.query.name === "nick"){
+      next();
+    } else {
+      res.status(HttpStatus.BAD_REQUEST).send("Not correct query name");
+    }
   }
 }

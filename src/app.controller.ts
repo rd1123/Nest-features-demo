93a90c616f22a42dba3e710a393232e6c4329075
rd <rdding1123@gmail.com>
2021-03-25 12:32:44 +0800

    Create exception filter demo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Get, HttpStatus, Query } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Controller, Get, HttpException, HttpStatus, Query, Res, UseFilters } from '@nestjs/common';
+import { query, Response } from 'express';
 import { AppService } from './app.service';
+import { HttpExceptionFilter } from './exception/filter/http-exception.filter';
+import { ForbiddenException } from './exception/forbidden.exception';
 
 @Controller("app")
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseFilters(HttpExceptionFilter)
+  async getException(
+    @Res() response: Response,
+    @Query() query
+  ): Promise<string> {
+    throw new HttpException({
+      statusCode: 403,
+      error: "Forbindden error",
+    }, HttpStatus.FORBIDDEN);
   };
 
   @Get("/name")
